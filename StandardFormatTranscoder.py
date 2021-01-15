f3a6a5a97a24528909d805998c193dffdb23e982
jacksalssome <jacksalssome@hotmail.com>
2021-01-15 16:17:13 +1000

    Add error message for double drive letters (AB:\)

--- StandardFormatTranscoder.py
+++ StandardFormatTranscoder.py
@@ -91,10 +91,11 @@ elif os.path.exists(args.input):
             else:
                 print("Please enter yes or no.")
 else:
-    if "yes" == re.sub("[A-Z]:\"", "yes", str(args.input)) or args.input == "/":
-        if currentOS == "Windows":
+    if currentOS == "Windows" and len(args.input) <= 3:  # <= 3 equals C:\ (root dir)
             print(Fore.YELLOW + "Can't run in root of drive, input has to be like: " + Fore.RESET + "-i \"D:\\folder\"")
-        elif currentOS == "Linux":
+    elif currentOS == "Windows" and len(args.input) <= 4 and re.search("[A-Z][A-Z]:\\", str(args.input)):  # <= 4 equals AB:\ (root dir)
+            print(Fore.YELLOW + "Nice drive letters, but can't run in root of drive, input has to be like: " + Fore.RESET + "-i \"D:\\folder\"")
+    elif currentOS == "Linux" and len(args.input) <= 1:  # <= 2 equals / (root dir)
             print(Fore.YELLOW + "Can't run in root of drive, input has to be like: " + Fore.RESET + "-i \"/home\"")
     else:
         print(Fore.YELLOW + "Can't find file path: \"" + Fore.RESET + args.input + Fore.YELLOW + "\"" + Fore.RESET)
@@ -105,7 +106,6 @@ runRecursive = False
 if args.recursive == True:  # If -r or --recursive is present then enable recursive
     runRecursive = True
 elif args.recursive == False:
-    runRecursive = False
 
 runRename = False
 if args.rename == True:  # If --rename present, then enable auto renaming
